3d25026f1f34a55a6c3bea9378c03add1ee80947
David Dawson <david.dawson@dawsonsystems.com>
2011-12-20 14:43:44 +0000

    space for prod config

--- grails-app/conf/cms/DomainFilters.groovy
+++ grails-app/conf/cms/DomainFilters.groovy
@@ -9,7 +9,7 @@ class DomainFilters {
   def filters = {
     all(uri: ALL_EXCEPT_ADMIN, invert:true) {
       before = {
-        def requestedName = request.localName
+        def requestedName = request.getHeader('Host')
         println "Requested domain : $requestedName"
         request.space = WcmSpace.findByAliasURI(requestedName,[cache:true])
       }
