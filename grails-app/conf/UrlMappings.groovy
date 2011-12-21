34eadbf1813c97152dabe03e24237f48ad0d950b
David Dawson <david.dawson@dawsonsystems.com>
2011-12-21 11:20:28 +0000

    index view.

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,13 +1,6 @@
 class UrlMappings {
 
 	static mappings = {
-		/*"/$controller/$action?/$id?"{
-			constraints {
-				// apply constraints here
-			}
-		}*/
-
-		"/"(view:"/index")
-		//"500"(view:'/error')
+		"/"(controller:"home", action:"index")
 	}
 }
