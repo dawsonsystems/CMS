99006ceae136c2d6be8b8232e7042a8f4b9954bf
David Dawson <david.dawson@dawsonsystems.com>
2011-12-22 12:50:50 +0000

    Add shiro security

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,21 @@
+import org.apache.shiro.crypto.hash.Sha256Hash
+
 class BootStrap {
 
     def init = { servletContext ->
+
+      if (!ShiroRole.findByName("admin")) {
+        log.info("Generating Admin Role")
+        ShiroRole userRole = new ShiroRole(name:"admin")
+        userRole.addToPermissions("*:*")
+        userRole.save()
+
+        log.info("Generating Admin User")
+        def user = new ShiroUser(username: "david", passwordHash: new Sha256Hash("examplepass").toHex())
+        user.addToPermissions("*:*")
+        user.addToRoles(userRole)
+        user.save(flush:true)
+      }
     }
     def destroy = {
     }
--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -2,5 +2,7 @@ class UrlMappings {
 
 	static mappings = {
 		"/"(controller:"home", action:"index")
+    "/auth/login"(controller:"auth", action:"login")
+    "/auth/lostPassword"(controller:"auth", action:"lostPassword")
 	}
 }
